b2e93cdc4586a3686f328587a11ef4758a92b2cd
339761330 <339761330@2260I-L29-021>
2024-09-05 09:29:16 -0400

    Add project files.

---
+++ Topic 1 - The Basics/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_1___The_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Emmett Cornies
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I will print a new line");
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Hello World!");
+            Console.Write("What is different?");
+        }
+    }
+}
